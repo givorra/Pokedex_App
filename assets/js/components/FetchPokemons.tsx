e619d37dee24d31e8e766612d86875d7d6f0c424
gacel <gir4@alu.ua.es>
2017-11-06 21:56:04 +0100

    Filtrando pokemons por nombre

--- assets/js/components/FetchPokemons.tsx
+++ assets/js/components/FetchPokemons.tsx
@@ -20,57 +20,76 @@ interface Pokemon {
 
 interface FetchPokemonsState {
   pokemons: Pokemon[]
+  pokemons2show: Pokemon[]
   loading: boolean
+  name_filter: string
 }
 
 export default class FetchPokemons extends React.Component<{}, FetchPokemonsState> {
   constructor() {
     super()
-    this.state = { pokemons: [], loading: true }
-
+    this.state = { pokemons: [],  pokemons2show: [], loading: true, name_filter: "Gacel"}
+    this.handleChange = this.handleChange.bind(this);
     // Get the data from our API.
     fetch('/api/pokemons')
       .then((response) => response.json() as Promise<ApiResponse>)
       .then((data) => {
-        this.setState({ pokemons: data.data, loading: false })
+        this.setState({ pokemons: data.data,  pokemons2show: data.data, loading: false })
       })
   }
 
-  private static renderPokemonsTable(pokemons: Pokemon[]) {
+  handleChange(event) {
+    console.error("------------------------ Pepee -------------------------\n\n")
+    //this.state.pokemons.filter(key => this.state.pokemons[key].name == "Gacel")
+    //this.state.pokemons = {} as Pokemon[]
+    //this.setState({pokemons: this.state.pokemons.filter(function(item) {return item.name >= "Gacel"})})
+    //alert(event.target.value)
+    var pokemons2show = this.state.pokemons.filter(function(item) {return item.name.toLowerCase().match(event.target.value.toLowerCase())})
+    //this.setState({pokemons: []})
+    this.setState({pokemons2show: pokemons2show})
+  }
+
+  private renderPokemonsTable() {
     return (
-      <Table>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Evolution</th>
-            <th>Favourite</th>
-            <th>Type 1</th>
-            <th>Type 2</th>
-          </tr>
-        </thead>
-        <tbody>
-          {pokemons.map((pokemon) =>
-            <tr key={pokemon.id}>
-              <td>{pokemon.id}</td>
-              <td>{pokemon.name}</td>
-              <td>{pokemon.description}</td>
-              <td>{pokemon.evolution_to}</td>
-              <td>{pokemon.favourite}</td>
-              <td>{pokemon.tipe1}</td>
-              <td>{pokemon.tipe2}</td>
+      <form /*onSubmit={this.handleSubmit}*/>
+        <label>
+          Filter by name:
+          <input type="text" /*value={this.state.name_filter}*/ onChange={this.handleChange} />
+        </label>
+        <Table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Evolution</th>
+              <th>Favourite</th>
+              <th>Type 1</th>
+              <th>Type 2</th>
             </tr>
-          )}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {this.state.pokemons2show.map((pokemon) =>
+              <tr key={pokemon.id}>
+                <td>{pokemon.id}</td>
+                <td>{pokemon.name}</td>
+                <td>{pokemon.description}</td>
+                <td>{pokemon.evolution_to}</td>
+                <td>{pokemon.favourite}</td>
+                <td>{pokemon.tipe1}</td>
+                <td>{pokemon.tipe2}</td>
+              </tr>
+            )}
+          </tbody>
+        </Table>
+      </form>
     )
   }
 
   public render(): JSX.Element {
     const content = this.state.loading
       ? <p><em>Loading...</em></p>
-      : FetchPokemons.renderPokemonsTable(this.state.pokemons)
+      : this.renderPokemonsTable()
 
     return (
       <div>
