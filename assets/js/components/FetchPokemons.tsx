29c725b1d28c3971f65ffdae5dc76a1509654975
gacel <gir4@alu.ua.es>
2017-11-14 12:25:18 +0100

    Ventana modal de confirmación para borrar y respuesta de la operación

--- assets/js/components/FetchPokemons.tsx
+++ assets/js/components/FetchPokemons.tsx
@@ -1,12 +1,27 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 import { Card, CardImg, CardText, CardDeck, CardBody, CardTitle, CardSubtitle,
-  Table, Button, Col, ButtonGroup, Form, FormGroup, Input, Label } from 'reactstrap'
+  Table, Button, Col, ButtonGroup, Form, FormGroup, Input, Label,
+  Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 import Pokemon from '../interfaces/IPokemon'
 
+const modalDefault: IModal = { isOpen: false, title: "", body: "", hiddenBtnDelete: true,
+  hiddenBtnOk: true, hiddenBtnCancel: true, idPokemonSelected: 0}
+const titleDeletePokemon = "Delete Pokemon"
+const bodyDeletePokemon = "¿Are you sure you want to delete this pokemon?"
 // The interface for our API response
 interface ApiResponse {
   data: Pokemon[]
+}
+
+interface IModal {
+  isOpen: boolean
+  title: string
+  body: string
+  hiddenBtnDelete: boolean
+  hiddenBtnOk: boolean
+  hiddenBtnCancel: boolean
+  idPokemonSelected: number
 }
 
 interface FetchPokemonsState {
@@ -15,19 +30,27 @@ interface FetchPokemonsState {
   loading: boolean
   name_filter: string
   favourite_filter: boolean
+  modal: IModal
 }
 
 export default class FetchPokemons extends React.Component<any, FetchPokemonsState> {
   constructor() {
     super()
-    this.state = { pokemons: [],  pokemons2show: [], loading: true, name_filter: '', favourite_filter: false}
+    this.state = { pokemons: [],  pokemons2show: [], loading: true, name_filter: '',
+      favourite_filter: false, modal: modalDefault}
     this.handleNameFilterChange = this.handleNameFilterChange.bind(this);
     this.handleFavouriteFilterChange = this.handleFavouriteFilterChange.bind(this);
     this.handleNewPokemon = this.handleNewPokemon.bind(this);
+    this.handleModalClose = this.handleModalClose.bind(this);
+    this.handleModalDelete = this.handleModalDelete.bind(this);
 
   }
 
   componentWillMount() {
+    this.getPokemonsToState()
+  }
+
+  private getPokemonsToState() {
     this.setState({loading : true})
     // Get the data from our API.
     fetch('/api/pokemons')
@@ -63,6 +86,40 @@ export default class FetchPokemons extends React.Component<any, FetchPokemonsSta
     this.props.history.push('/create-pokemon')
   }
 
+  private handleDeletePokemonClick(id, event) {
+    let modal = Object.assign({}, modalDefault);
+    modal.isOpen = true
+    modal.title = titleDeletePokemon
+    modal.body = bodyDeletePokemon
+    modal.hiddenBtnDelete = false
+    modal.hiddenBtnCancel = false
+    modal.idPokemonSelected = id
+    this.setState({modal: modal})
+  }
+
+  private handleModalClose() {
+    this.setState({modal: modalDefault})
+  }
+
+  private handleModalDelete() {
+    let modal = Object.assign({}, modalDefault);
+    modal.title = titleDeletePokemon
+    modal.isOpen = true
+    modal.hiddenBtnOk = false
+
+    fetch('/api/pokemons/' + this.state.modal.idPokemonSelected, { method: "DELETE"})
+      .then((response) => {
+        if(response.ok)
+        {
+          modal.body = "Succes!"
+        }
+        else {
+          modal.body = "Error: " + response.status + " " + response.statusText
+        }
+        this.setState({ modal: modal})
+      })
+  }
+
   private renderPokemonItems() {
     return (
       <CardDeck>
@@ -74,7 +131,7 @@ export default class FetchPokemons extends React.Component<any, FetchPokemonsSta
               <CardTitle>{pokemon.name}</CardTitle>
               <CardText>{pokemon.description}</CardText>
               <ButtonGroup  className="w-100">
-                <Button color="danger" className="w-50">Delete</Button>
+                <Button color="danger" className="w-50" onClick={this.handleDeletePokemonClick.bind(this, pokemon.id)}>Delete</Button>
                 <Col sm={2} />
                 <Button color="primary" className="w-50" onClick={this.handlePokemonClick.bind(this, pokemon.id)}>View</Button>
               </ButtonGroup>
@@ -106,8 +163,30 @@ export default class FetchPokemons extends React.Component<any, FetchPokemonsSta
             </Label>
           </FormGroup>
         </Col>
+        <Col>
+          <FormGroup>
+            <Button color="primary" onClick={this.handleNewPokemon}>Add new</Button>
+          </FormGroup>
+        </Col>
       </Form>
       )
+  }
+
+  private renderModal() {
+    return (
+      <Modal isOpen={this.state.modal.isOpen} >
+        <ModalHeader>{this.state.modal.title}</ModalHeader>
+        <ModalBody>{this.state.modal.body}</ModalBody>
+        <ModalFooter>
+          <Button color="secondary" hidden={this.state.modal.hiddenBtnCancel}
+            onClick={this.handleModalClose}>Cancel</Button>
+          <Button color="primary" hidden={this.state.modal.hiddenBtnOk}
+            onClick={this.handleModalClose}>Ok</Button>
+          <Button color="primary" hidden={this.state.modal.hiddenBtnDelete}
+            onClick={this.handleModalDelete}>Delete</Button>
+        </ModalFooter>
+      </Modal>
+    )
   }
 
   public render(): JSX.Element {
@@ -116,9 +195,11 @@ export default class FetchPokemons extends React.Component<any, FetchPokemonsSta
       : this.renderPokemonItems()
 
     const filters = this.renderFilters()
+    const modal = this.renderModal()
 
     return (
       <div>
+        {modal}
         <h1>List of your pokemons</h1>
         <br />
         {filters}
